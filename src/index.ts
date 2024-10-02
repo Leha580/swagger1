c912531b3fa19ae56191cd97a4904c74afd36a4e
leha- vitor96 <viiiitorr96@gmail.com>
2024-10-02 15:37:38 +0900

    routes module

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,29 @@ import {addressesRouter} from "./routes/addresses-router";
 const app = express()
 const port = process.env.PORT || 5001
 
-
 const parserMiddleware = bodyParser({})
 app.use(parserMiddleware)
 
+
+const videos = [
+    {
+        "id": 0,
+        "title": "string",
+        "author": "string",
+        "canBeDownloaded": true,
+        "minAgeRestriction": null,
+        "createdAt": "2024-10-02T06:25:23.544Z",
+        "publicationDate": "2024-10-02T06:25:23.544Z",
+        "availableResolutions": [
+            "P144"
+        ]
+    }
+]
+
+app.delete('/testing/all-data', (req: Request, res: Response) => {
+
+    res.send(204)
+})
 app.use('/products', productsRouter)
 app.use('/addresses', addressesRouter)
 
